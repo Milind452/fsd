4f3f146f0497557d898713e15714098e0090c42f
Milind452 <mohapatra.milind@gmail.com>
2021-04-03 02:03:23 +0530

    Make instance for avatar

--- movies/entertainment_center.py
+++ movies/entertainment_center.py
@@ -7,7 +7,20 @@ if __name__ == '__main__':
 							'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
 							'https://www.youtube.com/watch?v=rNk1Wi8SvNc')
 
+	print('\n***Toy Story***')
 	print('Title:', toyStory.title)
 	print('Storyline:', toyStory.storyline)
 	print('Image:', toyStory.image)
 	print('Trailer:', toyStory.trailer)
+
+
+	avatar = media.Movie('Avatar',
+						'A marine on an alien planet',
+						'https://upload.wikimedia.org/wikipedia/en/b/b0/Avatar-Teaser-Poster.jpg',
+						'https://www.youtube.com/watch?v=5PSNL1qE6VY')
+
+	print('\n***Avatar***')
+	print('Title:', avatar.title)
+	print('Storyline:', avatar.storyline)
+	print('Image:', avatar.image)
+	print('Trailer:', avatar.trailer)
