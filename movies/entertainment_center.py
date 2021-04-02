43a32c8aa81d39ef15a38da80f2af90a596b1c76
Milind452 <mohapatra.milind@gmail.com>
2021-04-03 01:49:57 +0530

    Make toy story instance of Movie class

---
+++ movies/entertainment_center.py
@@ -0,0 +1,11 @@
+import media
+
+
+if __name__ == '__main__':
+	toyStory = media.Movie('Toy Story',
+							'Astory of a boy and his toys that come to life.',
+							'https://upload.wikimedia.org/wikipedia/en/1/13/Toy_Story.jpg',
+							'https://www.youtube.com/watch?v=rNk1Wi8SvNc')
+
+	print('Title:', toyStory.title)
+	print('Storyline:', toystory.storyline)
