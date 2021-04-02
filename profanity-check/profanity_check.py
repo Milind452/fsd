f6f1711120c712d5d84cf404ee8bed85ffa54a67
Milind452 <mohapatra.milind@gmail.com>
2021-04-03 00:02:19 +0530

    Complete profanity checker

--- profanity-check/profanity_check.py
+++ profanity-check/profanity_check.py
@@ -20,4 +20,9 @@ if __name__ == '__main__':
 	path = '/home/milind/Documents/fsd/profanity-check/movie_quotes.txt'
 	contents = readText(path)
 	response = checkProfanity(contents)
-	print(response)
+	if b'true' in response:
+		print('Profanity alert!!!')
+	elif b'false' in response:
+		print('This document has no curse words')
+	else:
+		print('Could scan the document properly')
