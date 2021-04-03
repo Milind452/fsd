88a3bca856c79c5afc91a2be451a90921ee7ba22
Milind452 <mohapatra.milind@gmail.com>
2021-04-03 17:51:13 +0530

    Add showTrailer functionality

--- movies/media.py
+++ movies/media.py
@@ -1,6 +1,12 @@
+import webbrowser
+
+
 class Movie():
 	def __init__(self, title, storyline, image, trailer):
 		self.title = title
 		self.storyline = storyline
 		self.image = image
 		self.trailer = trailer
+
+	def showTrailer(self):
+		webbrowser.open(self.trailer)
